5c0c6d9040d4d0acbd8ebd2939738603a7ce4b0c
Ivan Paraskun <ig.paraskun@gmail.com>
2023-11-25 19:53:24 +0700

    tests/construct

--- test/ringbuffer_tests.cpp
+++ test/ringbuffer_tests.cpp
@@ -4,6 +4,20 @@
 
 using namespace ringbuffer;
 
+TEST(RingBufferTests, ConstructValid) {
+    RingBuffer<int> buffer{10};
+
+    ASSERT_EQ(buffer.capacity(), 10);
+    ASSERT_EQ(buffer.size(), 0);
+}
+
+TEST(RingBufferTests, ContructZeroCapacity) {
+    RingBuffer<int> *buffer;
+    ASSERT_THROW(buffer = new RingBuffer<int>(0), std::invalid_argument);
+    ASSERT_EQ(buffer, nullptr);
+}
+
+
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
